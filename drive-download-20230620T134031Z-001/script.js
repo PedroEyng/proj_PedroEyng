a637cf900b164502cb5590fce5c972101260f687
PedroEyng <138597929+PedroEyng@users.noreply.github.com>
2023-07-04 11:35:20 -0300

    Add files via upload

---
+++ drive-download-20230620T134031Z-001/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scroll = new SmoothScroll('a[href*="#"]', {
+      speed: 800,
+    });
+  });
+  
---
+++ drive-download-20230620T134031Z-001/script2.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var arrow = document.querySelector(".seta-direita");
+
+  arrow.addEventListener("click", function() {
+    // Redirecionar para a nova página
+    window.location.href = "personalizaçoes.html";
+  });
+});
